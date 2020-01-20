70309eb3b71376a93b62b0b677bb931f464dda52
James Trinder <jamesanthonyt@live.co.uk>
2020-01-20 13:54:15 +0000

    Created theatre seeds for development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Theatre.create(
+  name: 'Theatre Royal Somewhere',
+  managed_by: '',
+  af_source_org_id: 1023,
+  notes: 'These guys are great!'
+)
+Theatre.create(
+  name: 'The Somewhere Palace',
+  managed_by: '',
+  af_source_org_id: 1027,
+  notes: 'These guys are annoying'
+)
+Theatre.create(
+  name: 'Studio Random',
+  managed_by: '',
+  af_source_org_id: 1030,
+  notes: 'Require some hand holding'
+)
