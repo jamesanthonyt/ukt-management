d77315e55b5b0fadde2e850c63fbafd4480311c5
James Trinder <jamesanthonyt@live.co.uk>
2020-01-31 18:09:42 +0000

    implemented begining of multiple update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   resources :theatres do
     resources :performance_spaces
-    resources :af_venues, only: [:edit, :update]
-
+    resources :af_venues, only: :index do
+      collection do
+        put :map_performance_space
+      end
+    end
   end
 end
